cdb3f7624c23f897d25f6f045b71df20f7dc3327
mateusz.gorecki <mategor195@student.polsl.pl>
2025-01-29 20:18:09 +0100

    LvL E 0.5

--- Assets/Resources/Level_A/HouseManager.cs
+++ Assets/Resources/Level_A/HouseManager.cs
@@ -9,6 +9,8 @@ public class HouseManager : MonoBehaviour
 {
     // Lista przechowuj¹ca obiekty HouseStreet
     public List<HouseStreet> HouseList = new List<HouseStreet>();
+    public GameObject PackagePrefab;
+    public Transform PackegeSpawnPoint;
 
     // Metoda dodaj¹ca nowy dom do listy
     public void AddHouse(HouseStreet house)
@@ -42,16 +44,31 @@ public class HouseManager : MonoBehaviour
         return HouseList[randomIndex];
     }
 
+
     // Przyk³ad u¿ycia w Start
     void Start()
     {
+        HouseStreet.onPackageDelivered += PackageSpawn;
         HouseList = FindObjectsByType<HouseStreet>(FindObjectsSortMode.InstanceID).ToList();
+
+        foreach (HouseStreet house in HouseList)
+        {
+            house.SetMailbox(false);
+        }
         // Losowanie i wyœwietlanie informacji o losowym domu
+        PackageSpawn();
+    }
+
+    private void PackageSpawn()
+    {
         HouseStreet randomHouse = RandomizeHouse();
         if (randomHouse != null)
         {
             Debug.Log("Randomized House:");
             randomHouse.DisplayInfo();
+            randomHouse.SetMailbox(true);
+            GameObject pack = Instantiate(PackagePrefab, PackegeSpawnPoint.position,Quaternion.identity);
+            pack.GetComponent<Package>().SetPackage(randomHouse.HouseNumber, randomHouse.StreetColor);
         }
     }
 }
--- Assets/Resources/Level_A/HouseStreet.cs
+++ Assets/Resources/Level_A/HouseStreet.cs
@@ -1,11 +1,32 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class HouseStreet : MonoBehaviour
 {
+    public static Action onPackageDelivered;
     public int HouseNumber;
     public Color StreetColor;
     public GameObject Mailbox;
+    public TMP_Text HouseNo;
+    public Image HouseColorImage;
+
+    private void Awake()
+    {
+        HouseNo.text = HouseNumber.ToString();
+        HouseColorImage.color = StreetColor;
+        Mailbox.GetComponent<XRSocketInteractor>().selectEntered.AddListener(PackageDelivered);
+    }
+
+    private void PackageDelivered(SelectEnterEventArgs arg0)
+    {
+        onPackageDelivered.Invoke();
+        Debug.Log("PackageDelivered");
+        Destroy(arg0.interactableObject.transform);
+    }
 
     public HouseStreet(int houseNumber, Color streetColor)
     {
